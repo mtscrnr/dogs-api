4015380e8ac21c57a771b65213c6250a7d6260f9
Matheus Cunha Carneiro <matheuscunhacarneiro@MacBook-Pro-de-Matheus.local>
2022-12-27 10:48:55 -0300

    first commit

---
+++ src/test/java/mtscarneiro/dogs/app/DogApplicationTests.java
@@ -0,0 +1,13 @@
+package mtscarneiro.dogs.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
